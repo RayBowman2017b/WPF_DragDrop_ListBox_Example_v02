e22c82154dbfbc3d5cff5c96c5b49505cca9b82d
RayBowman2017b <32105857+RayBowman2017b@users.noreply.github.com>
2019-01-06 11:53:08 -0600

    first commit

---
+++ Reorder_item_WPF/MainViewModel.cs
@@ -0,0 +1,125 @@
+﻿using GongSolutions.Wpf.DragDrop;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Reorder_item_WPF
+{
+
+public class MyDropHandler : IDropTarget
+    {
+
+        void IDropTarget.DragOver(DropInfo dropInfo)
+        {
+            if (dropInfo.Data is MSP)
+            {
+                dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
+                dropInfo.Effects = DragDropEffects.Move;
+            }
+        }
+
+        void IDropTarget.Drop(DropInfo dropInfo)
+        {
+           
+            MSP msp = (MSP)dropInfo.Data;
+            ((IList)dropInfo.DragInfo.SourceCollection).Remove(msp);
+
+        }
+
+        //public MyDropHandler(MainWindowViewModel mainWindowViewModel)
+        //{
+        //    _mainWindowViewModel = mainWindowViewModel;
+        //}
+
+        //public void DragOver(IDropInfo dropInfo)
+        //{
+        //    dropInfo.Effects = DragDropEffects.Copy;
+        //}
+
+        //public void Drop(IDropInfo dropInfo)
+        //{
+        //    _mainWindowViewModel.Name = (string) dropInfo.Data;
+        //}
+    }    
+
+
+
+
+    class MainViewModel : IDropTarget
+    {
+        public ObservableCollection<MSP> MSPCollection { get; set; }
+
+        private int V_button_ndx;
+
+        public MainViewModel()
+        {
+            MSPCollection = new ObservableCollection<MSP>();
+
+            /*  *  /
+            MSPCollection.Add(new MSP() { 
+                Id = 1,
+                Name = "Anis Derbel"
+            });
+
+            MSPCollection.Add(new MSP()
+            {
+                Id = 2,
+                Name = "Firas Mdimagh"
+            });
+
+            MSPCollection.Add(new MSP()
+            {
+                Id = 3,
+                Name = "Khaled Jemni"
+            });
+
+            MSPCollection.Add(new MSP()
+            {
+                Id = 4,
+                Name = "Sahbouch"
+            });
+            /*  */
+
+            for (V_button_ndx=1; V_button_ndx < 22; V_button_ndx++)
+                MSPCollection.Add(new MSP()
+                {
+                    Id = V_button_ndx,
+                    Name = "Button 00" + V_button_ndx
+                });
+        }
+
+        public void add_new_element ()
+        {
+            MSP L_new_item = new MSP()
+                {
+                    Id = V_button_ndx,
+                    Name = "Button 00" + V_button_ndx
+                };
+
+            V_button_ndx++;
+
+            MSPCollection.Add(L_new_item);
+        }
+
+        void IDropTarget.DragOver(DropInfo dropInfo)
+        {
+            if (dropInfo.Data is MSP)
+            {
+                dropInfo.DropTargetAdorner = DropTargetAdorners.Highlight;
+                dropInfo.Effects = DragDropEffects.Move;
+            }
+        }
+
+        void IDropTarget.Drop(DropInfo dropInfo)
+        {
+           
+            MSP msp = (MSP)dropInfo.Data;
+            ((IList)dropInfo.DragInfo.SourceCollection).Remove(msp);
+        }
+    }
+}
---
+++ Reorder_item_WPF/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reorder_item_WPF
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new MainViewModel();
+        }
+
+        private void ScrollVwr_for_main_content_GotFocus(object sender,RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_add_new_button_Click(object sender,RoutedEventArgs e)
+        {
+        }
+
+        private void BTN_close_this_window_Click(object sender,RoutedEventArgs e)
+        {
+            this.Close ();
+        }
+
+        private void BTN_new_file_Click(object sender,RoutedEventArgs e)
+        {
+            ((MainViewModel)this.DataContext).add_new_element ();
+        }
+
+        private void Drop_button(object sender,DragEventArgs e)
+        {
+            int x = 1;
+        }
+
+        private void Button_Drop(object sender,DragEventArgs e)
+        {
+            int x = 1;
+        }
+
+        private void Button_drag_Drop(object sender,DragEventArgs e)
+        {
+            int x = 0;
+        }
+
+        int a_cnt = 0;
+        private void Button_DragLeave(object sender,DragEventArgs e)
+        {
+            a_cnt++;
+        }
+
+        private void Button_Click(object sender,RoutedEventArgs e)
+        {
+            int x=0;
+            a_cnt=0;
+        }
+
+private void Grid_DragOver(object sender, DragEventArgs e)
+{
+    //e.AcceptedOperation = DataPackageOperation.Copy;
+    //e.AllowedEffects = DragDropEffects.Move;
+}
+
+    }
+}
